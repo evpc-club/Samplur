acda43eed9eb0346c2e46ef22376cb1f8a49abcb
AydanPirani <aydanpirani@gmail.com>
2020-09-14 23:31:32 -0700

    Simplified logic in collecting inputs.

--- main.py
+++ main.py
@@ -29,73 +29,82 @@ f = open("data.txt", "r")
 for x in f:
     file.append(int(x))
 
-sample_size = int(input("Please enter the sample size: ")) #Take the sample
-
+sample_size = 10
+# sample_size = int(input("Please enter the sample size: ")) #Take the sample
 while sample_size > len(file):
     sample_size = int(input("\nPlease try again. Your sample size is larger than the amount of data: "))
 
-repetitions = int(input("\nPlease enter the number of times you want this to repeat: "))
-means = []
+repetitions = 4
+# repetitions = int(input("\nPlease enter the number of times you want this to repeat: "))
+
+means = {} #Dictionary. Key = mean, value = amount
 
 #Record the means and add to aggregate list
-for i in range(repetitions-1):
-    currentData = random.sample(file, sample_size)
-    means.append(sum(currentData)/sample_size)
-
-graph = [[], []]
-means.sort()
-for i in range(len(means)):
-    means[i] = round(means[i]*10)/10
-
-if lenOfIntArr(means) < 102:
-    currentValue = means[0]
-    graph[0].append(str(currentValue))
-    graph[1].append(1)
-    for i in range(len(means)):
-        if means[i] == currentValue:
-            graph[1][len(graph[1]) - 1] += 1
-        else:
-            currentValue = means[i]
-            graph[0].append(str(currentValue))
-            graph[1].append(1)
-    plt.figure(1)
-    y_pos = np.arange(len(graph[0]))
-    plt.bar(y_pos, graph[1], align='center', alpha=0.5)
-    plt.xticks(y_pos, graph[0])
-    plt.ylabel('Occurrences')
-    plt.title('Statistics')
-else:
-    lastMean = means[len(means) - 1]
-    rangeLen = float(int(lastMean)+1-int(means[0]))
-    if float(int(lastMean)) == lastMean:
-        rangeLen -= 1
-    rangeLen = rangeLen/float(14)
-    if float(int(rangeLen)) == rangeLen:
-        rangeLen = int(rangeLen)
+for i in range(repetitions):
+    temp_mean = sum(random.sample(file, sample_size))/sample_size
+    if temp_mean in means.keys():
+        means[temp_mean] += 1
     else:
-        rangeLen = int(rangeLen) + 1
-    i = int(means[0]) - (int(means[0]) % rangeLen)
-    newMeans = []
-    newOccurrences = []
-    while i <= int(lastMean) - (int(lastMean) % rangeLen):
-        newMeans.append(str(i) + "-" + str(i + rangeLen))
-        newOccurrences.append(len(partInRange(i, i+rangeLen, means)))
-        i += rangeLen
-    plt.figure(1)
-    y_pos = np.arange(len(newMeans))
-    plt.bar(y_pos, newOccurrences, align='center', alpha=0.5)
-    plt.xticks(y_pos, newMeans)
-    plt.ylabel('Occurrences')
-    plt.title('Statistics')
-    plt.figure(2)
-    y_pos = np.arange(len(newMeans))
-    percentOfData = []
-    for i in range(len(newOccurrences)):
-        percentOfData.append(newOccurrences[i]/sum(newOccurrences)*100)
-    plt.bar(y_pos, percentOfData, align='center', alpha=0.5)
-    plt.xticks(y_pos, newMeans)
-    plt.ylabel('% of Data')
-    plt.title('Statistics')
+        means[temp_mean] = 1
 
+print(means)
 
-plt.show()
+# graph = [[], []] #List of lists. x and y coordinates
+# means.sort()
+# for i in range(len(means)):
+#     means[i] = round(means[i]*10)/10
+
+# plt.bar(y_pos, align='center', alpha=0.5)
+
+# if lenOfIntArr(means) < 102:
+#     currentValue = means[0]
+#     graph[0].append(str(currentValue))
+#     graph[1].append(1)
+#     for i in range(len(means)):
+#         if means[i] == currentValue:
+#             graph[1][len(graph[1]) - 1] += 1
+#         else:
+#             currentValue = means[i]
+#             graph[0].append(str(currentValue))
+#             graph[1].append(1)
+#     plt.figure(1)
+#     y_pos = np.arange(len(graph[0]))
+#     plt.bar(y_pos, graph[1], align='center', alpha=0.5)
+#     plt.xticks(y_pos, graph[0])
+#     plt.ylabel('Occurrences')
+#     plt.title('Statistics')
+# else:
+#     lastMean = means[len(means) - 1]
+#     rangeLen = float(int(lastMean)+1-int(means[0]))
+#     if float(int(lastMean)) == lastMean:
+#         rangeLen -= 1
+#     rangeLen = rangeLen/float(14)
+#     if float(int(rangeLen)) == rangeLen:
+#         rangeLen = int(rangeLen)
+#     else:
+#         rangeLen = int(rangeLen) + 1
+#     i = int(means[0]) - (int(means[0]) % rangeLen)
+#     newMeans = []
+#     newOccurrences = []
+#     while i <= int(lastMean) - (int(lastMean) % rangeLen):
+#         newMeans.append(str(i) + "-" + str(i + rangeLen))
+#         newOccurrences.append(len(partInRange(i, i+rangeLen, means)))
+#         i += rangeLen
+#     plt.figure(1)
+#     y_pos = np.arange(len(newMeans))
+#     plt.bar(y_pos, newOccurrences, align='center', alpha=0.5)
+#     plt.xticks(y_pos, newMeans)
+#     plt.ylabel('Occurrences')
+#     plt.title('Statistics')
+#     plt.figure(2)
+#     y_pos = np.arange(len(newMeans))
+#     percentOfData = []
+#     for i in range(len(newOccurrences)):
+#         percentOfData.append(newOccurrences[i]/sum(newOccurrences)*100)
+#     plt.bar(y_pos, percentOfData, align='center', alpha=0.5)
+#     plt.xticks(y_pos, newMeans)
+#     plt.ylabel('% of Data')
+#     plt.title('Statistics')
+#
+#
+# plt.show()
