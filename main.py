0c46dd585c468f5ba5ea19e35c696cc58124dafb
AydanPirani <aydanpirani@gmail.com>
2020-09-22 00:37:20 -0700

    Refactored backend.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import random, math
+import random, math, os
 plt.rcdefaults()
 
 #Return new rounded dictionary
@@ -12,57 +12,74 @@ def round_dict(dict, round_value):
         result[new_key] = temp
     return result
 
+#Return buckets based on frequency
+def generate_axes(data, buckets):
+    max_mean = int(max(data))
+    min_mean = int(min(data))
+    round_value = round(int(math.log(max_mean/min_mean) - 1), 1) #Constant equation to round. Defined by Yashika.
+    bucket_size = math.ceil((max_mean-min_mean)/buckets)
+    bucket_number = 0
+    leftover_count = 0
+
+    x_axis = []
+    y_axis = []
+
+    data.sort()
+
+    i = 0
+    for b in range(buckets):
+        start = b * bucket_size + min_mean
+        end = (b+1) * bucket_size + min_mean
+        x_axis.append(str(start) + "-" + str(end))
+        y_axis.append(0)
+
+        while i < len(data) and data[i] < end:
+            y_axis[b] += 1
+            i += 1
+
+    return x_axis, y_axis
+
 def add_labels(rects, max_frequency):
-    smallest_x = 0
     for rect in rects:
         height = rect.get_height()
         if height == max_frequency:
-            if (rect.get_x() - smallest_x) > 20:
-                smallest_x = rect.get_x()
-                ax.text(rect.get_x() + rect.get_width()/2., height,'%d' % int(rect.get_x()),ha='center', va='bottom')
-            elif (rect.get_x() - smallest_x) > 10:
-                ax.text(rect.get_x() + rect.get_width()/2., 1.03 * height,'%d' % int(rect.get_x()),ha='center', va='bottom')
-file = []
-f = open("data.txt", "r")
+            ax.text(rect.get_x() + rect.get_width()/2., height,height,ha='center', va='bottom')
+        elif max_frequency == -1:
+            ax.text(rect.get_x() + rect.get_width()/2., height,height,ha='center', va='bottom')
+
+data = []
+path = os.path.dirname(os.path.realpath(__file__)) + "\\"
+f = open(path+"data.txt", "r")
 for x in f:
-    file.append(int(x))
+    data.append(int(x))
 
 sample_size = 10 #Hard-coded value for testing purposes
 # sample_size = int(input("Please enter the sample size: ")) #Take the sample
-while sample_size > len(file):
+while sample_size > len(data):
     sample_size = int(input("\nPlease try again. Your sample size is larger than the amount of data: "))
 
-repetitions = 100 #Hard-coded value for testing purposes
+repetitions = 10000 #Hard-coded value for testing purposes
 # repetitions = int(input("\nPlease enter the number of times you want this to repeat: "))
 
-means = {} #Dictionary. Key = mean, value = amount
+buckets = 50 #Hard-coded value for testing purposes
+# buckets = int(input("\nPlease enter the amount of buckets (note than going above 50 may result in performance issues): "))
 
-for i in range(repetitions): #Record the means and add to aggregate list
-    temp_mean = sum(random.sample(file, sample_size))/sample_size
-    temp_mean = round(temp_mean, 0)
-    if temp_mean in means.keys(): #Check if the pre-existing mean exists in dictionary
-        means[temp_mean] += 1
-    else:
-        means[temp_mean] = 1 #Initialize dict[temp_mean] to have frequency 1
-
-max_mean = int(max(means.keys()))
-min_mean = int(min(means.keys()))
-
-round_value=round(int(math.log(max_mean/min_mean) - 1), 1) #Constant equation to round. Defined by Yashika.
-means = round_dict(means,round_value)
-
-x_axis = means.keys()
-y_axis = [means[key] for key in means] #Y-axis is the dictionary value (frequency) of every value in x
-
-x_ticks = range(min_mean,max_mean,(max_mean-min_mean)//10)
+means = [] #List of all means
+means.extend([round(sum(random.sample(data, sample_size))/sample_size, 0) for i in range(repetitions)])
+x_axis, y_axis = generate_axes(means, buckets)
 
 plt.rcParams.update({'font.size': 8})
 fig, ax = plt.subplots()
-bar1 = ax.bar(x_axis, y_axis, width=3,  align='center', alpha=0.5)
-plt.xticks(x_ticks)
+graph = ax.bar(x_axis, y_axis, width=0.5,  align='center')
 plt.ylabel('Occurrences')
-# plt.title('Statistics')
+plt.xlabel('Mean Ranges')
+plt.title('Statistics')
 
-add_labels(bar1, max(y_axis))
+if buckets < 30:
+    add_labels(graph, -1)
+    ax.set_xticklabels(x_axis, rotation=60)
+else:
+    add_labels(graph, max(y_axis))
+    ax.set_xticklabels([])
 
 plt.show()
